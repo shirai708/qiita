87e413bed6ea891417e5a2b924db76ee09ffc1c0
H. Watanabe <kaityo@users.sourceforge.jp>
2019-05-05 00:26:49 +0900

    revises thermal.py

--- linear/thermal.py
+++ linear/thermal.py
@@ -1,29 +1,28 @@
-import matplotlib
-matplotlib.use('Agg') 
-import matplotlib.pyplot as plt
-import numpy as np
 import copy
 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
 
-def calc(v, v2, h):
+
+
+def calc(v, h):
+    v2 = copy.copy(v)
     N = len(v)
     for i in range(N):
         i1 = (i+1) % N
         i2 = (i - 1 + N) % N
-        v2[i] = v[i] + (v[i1] - 2.0*v[i] + v[i2])*h
+        v[i] = v2[i] + (v2[i1] - 2.0*v2[i] + v2[i2])*h
 
 
 N = 32
-v = np.array([min(x,N-x) for x in range(N)],dtype='float64')
-v2 = np.zeros(N)
+v = np.array([min(x, N-x) for x in range(N)], dtype='float64')
 
 h = 0.1
 r = []
 for i in range(1000):
-    if (i % 2) == 0:
-        calc(v, v2, h)
-    else:
-        calc(v2, v, h)
+    calc(v, h)
     if (i % 100) == 0:
         r.append(copy.copy(v))
 
