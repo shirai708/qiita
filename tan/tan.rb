c8a6839df1ef1d22bd5379f3b51a7ca95d73c52c
H. Watanabe <kaityo@users.sourceforge.jp>
2019-07-01 09:15:54 +0900

    コード修正

--- tan/tan.rb
+++ tan/tan.rb
@@ -2,8 +2,8 @@ def diff(h)
   h2 = Hash.new(0)
   h.each do |key, v|
     m, n = key
-    h2[[m-1,n+1]] += v*n
-    h2[[m+1,n-1]] -= v*m 
+    h2[[m-1,n+1]] += v*n if !m.zero?
+    h2[[m+1,n-1]] -= v*m if !n.zero?
   end
   h2
 end
